7194071f41754755950a5e86ba2f4673a9ebf653
Jack Murley <jack.murley@gmail.com>
2024-04-28 19:18:53 +1000

    docs(ba): fixing broken links

--- classic/docusaurus.config.ts
+++ classic/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'JTMurley', // Usually your GitHub org/user name.
   projectName: 'born-again-wiki', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
